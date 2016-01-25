51107d3e7f5b69cd791e0fc152926fbf1e6406a9
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-25 01:16:44 +0000

    add exception handler

--- src/index.js
+++ src/index.js
@@ -12,6 +12,15 @@ export default function (factory, getInitialState) {
     if (getInitialState !== undefined)
       self.state = getInitialState(self);
     factory(self);
+    // Wrap an exception handler around the render function.
+    const originalRender = self.render;
+    self.render = function () {
+      try {
+        return originalRender();
+      } catch (ex) {
+        return <div className="epic-fail">{ex.toString()}</div>;
+      }
+    };
     return self;
   };
 };
